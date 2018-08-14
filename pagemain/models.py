aeededb8acb407329fd28bb345de8dc6d81c225d
andrey <andreyg4@yandex.ru>
2018-08-14 17:19:21 +0200

    change_140818

--- pagemain/models.py
+++ pagemain/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class FileImage(models.Model):
+    """Название изображения, которое необоходимо обработать"""
+    name_image = models.CharField(max_lenght=200, verbose_name='Название изображения')
+    name_file = models.FileField()
+    data_added = models.DataTimeField(auto_now_add=True, verbose_name='Дата создания изображения')
+    
+    class Meta:
+        verbose_name = 'Изображение'
+        verbose_name_plural = 'Изображения'
+        
+    def __str__(self):
+        return self.name_image
